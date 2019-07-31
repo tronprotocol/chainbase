be5160449ed3e6c8eaadd461e2783b54c9743cf8
wubin1 <wubin1@tron.network>
2019-07-31 15:40:00 +0800

    add dynamic store

---
+++ src/main/java/org/tron/common/utils/Utils.java
@@ -0,0 +1,9 @@
+package org.tron.common.utils;
+
+public class Utils {
+  public static byte[] clone(byte[] value) {
+    byte[] clone = new byte[value.length];
+    System.arraycopy(value, 0, clone, 0, value.length);
+    return clone;
+  }
+}
